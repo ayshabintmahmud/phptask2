f5e987daea804c316b4cc8a90130ca8d6bacf003
ayshabintmahmud <aysherbintmahmud@gmail.com>
2020-04-08 22:49:07 +0100

    initial commit

---
+++ lib/header.php
@@ -0,0 +1,15 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>AyshaBintMahmud Academy</title>
+  </head>
+  <body>
+    <div >
+      <h1 text-align: center; >AyshaBintMahmud Academy</h1>
+      <hr>
+    </div>
+
+  </body>
+  </html>
